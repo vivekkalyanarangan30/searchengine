850bb65b8ac4ebf00083e9b7e23e3cd2829fbf05
Vivek Kalyanarangan <vivekkalyanarangan30@gmail.com>
2018-08-31 10:44:57 +0400

    Update indexing.py
    bulk indexing - faster

--- python/indexing.py
+++ python/indexing.py
@@ -9,22 +9,26 @@ import pandas as pd
 import numpy as np
 import json
 import time
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 start_time = time.time()
-data = pd.read_csv('/usr/local/searchengine/data/people_wiki.csv')
+# data = pd.read_csv('/usr/local/searchengine/data/people_wiki.csv')
 
-print 'Data prepared in ' + str((time.time()-start_time)/60) + ' minutes'
+# print 'Data prepared in ' + str((time.time()-start_time)/60) + ' minutes'
 
-json_body = data.reset_index().to_json(orient='index')
-json_body = json_body.decode('ascii','ignore').encode('utf-8','replace')
-json_parsed = json.loads(json_body)
-print np.shape(data)
-for elements in json_parsed:
-    data_json = json_parsed[elements]
-    id_ = data_json['URI']
-    es.index(index='wiki_search', doc_type='data', id=id_, body=data_json)
-    print id_ + ' indexed successfully'
-print 'Indexed in '+str((time.time()-start_time)/60)+' minutes'
+# json_body = data.reset_index().to_json(orient='index')
+# json_body = json_body.decode('ascii','ignore').encode('utf-8','replace')
+# json_parsed = json.loads(json_body)
+# print np.shape(data)
+# for elements in json_parsed:
+#     data_json = json_parsed[elements]
+#     id_ = data_json['URI']
+#     es.index(index='wiki_search', doc_type='data', id=id_, body=data_json)
+#     print id_ + ' indexed successfully'
+# print 'Indexed in '+str((time.time()-start_time)/60)+' minutes'
+
+with open('/usr/local/searchengine/data/people_wiki.csv') as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index='wiki_search', doc_type='data')
